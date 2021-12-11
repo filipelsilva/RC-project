57d9a61b84d00cc3ddc031e874d3787c18d347eb
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-11 16:36:16 +0000

    now all working, just copied from the slides. now to improve the servers and such

--- client/tcp_client.c
+++ client/tcp_client.c
@@ -3,56 +3,43 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 
-#define PORT "58001"
+int main(void)
+{
+	struct addrinfo hints,*res;
+	int fd, n;
+	ssize_t nbytes,nleft,nwritten,nread;
+	char *ptr,buffer[128];
 
-int main(int argc, char **argv) {
-	int fd, errcode;
-	ssize_t n, nbytes, nleft, nwritten, nread;
-	socklen_t addrlen;
-	struct addrinfo hints, *res;
-	struct sockaddr_in addr;
-	char buffer[128];
-	char *ptr, *server;
+	fd=socket(AF_INET,SOCK_STREAM,0);//TCP socket
+	if(fd==-1)exit(1);//error
+	memset(&hints,0,sizeof hints);
+	hints.ai_family=AF_INET;//IPv4
+	hints.ai_socktype=SOCK_STREAM;//TCP socket
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd == -1) /* error */ exit(1);
+	n=getaddrinfo("localhost","58001",&hints,&res);
+	if(n!=0)/*error*/exit(1);
+	n=connect(fd,res->ai_addr,res->ai_addrlen);
+	if(n==-1)/*error*/exit(1);
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
+	ptr=strcpy(buffer,"Hello!\n");
+	nbytes=7;
 
-	server = "localhost";
-	errcode = getaddrinfo(server, PORT, &hints, &res);
-	if (errcode != 0) /* error */ exit(1);
-
-	n = connect(fd, res->ai_addr, res->ai_addrlen);
-	if (n == -1) /* error */ exit(1);
-
-	ptr = strcpy(buffer, "Hello!\n");
-	nbytes = strlen(ptr);
-	nleft = nbytes;
-	while (nleft > 0) {
-		nwritten = write(fd, ptr, nleft);
-		if (nwritten <= 0) /* error */ exit(1);
-		nleft -= nwritten;
-		ptr += nwritten;
-	}
-
-	nleft = nbytes; ptr=buffer;
-	while (nleft > 0) {
-		nread = read(fd, ptr, nleft);
-		if (nread == -1) /* error */ exit(1);
-		else if (nread == 0) break; // closed by peer
-		nleft -= nread;
-		ptr += nread;
-	}
-	nread = nbytes - nleft;
-
-	write(1, "echo: ", 6); write(1, buffer, n);
-
-	freeaddrinfo(res);
+	nleft=nbytes;
+	while(nleft>0){nwritten=write(fd,ptr,nleft);
+		if(nwritten<=0)/*error*/exit(1);
+		nleft-=nwritten;
+		ptr+=nwritten;}
+	nleft=nbytes; ptr=buffer;
+	while(nleft>0){nread=read(fd,ptr,nleft);
+		if(nread==-1)/*error*/exit(1);
+		else if(nread==0)break;//closed by peer
+		nleft-=nread;
+		ptr+=nread;}
+	nread=nbytes-nleft;
+	close(fd);
+	write(1,"echo: ",6);//stdout
+	write(1,buffer,nread);
+	exit(0);
 }
--- server/tcp_server.c
+++ server/tcp_server.c
@@ -1,59 +1,42 @@
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
+#include <string.h>
+#include <unistd.h>
 
-#define PORT "58001"
-
-int main(int argc, char **argv) {
-	int fd, newfd, errcode;
-	ssize_t n, nbytes, nleft, nwritten, nread;
-	socklen_t addrlen;
-	struct addrinfo hints, *res;
+int main(void)
+{
+	struct addrinfo hints,*res;
+	int fd,newfd,errcode;
+	ssize_t n,nw;
 	struct sockaddr_in addr;
-	char buffer[128];
-	char* ptr;
+	socklen_t addrlen;
+	char *ptr,buffer[128];
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd == -1) /* error */ exit(1);
+	if((fd=socket(AF_INET,SOCK_STREAM,0))==-1)exit(1);//error
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_DGRAM;
+	memset(&hints,0,sizeof hints);
+	hints.ai_family=AF_INET;//IPv4
+	hints.ai_socktype=SOCK_STREAM;//TCP socket
 	hints.ai_flags=AI_PASSIVE;
+	if((errcode=getaddrinfo(NULL,"58001",&hints,&res))!=0)/*error*/exit(1);
 
-	errcode = getaddrinfo(NULL, PORT, &hints, &res);
-	if (errcode != 0) /* error */ exit(1);
+	if(bind(fd,res->ai_addr,res->ai_addrlen)==-1)/*error*/exit(1);
+	if(listen(fd,5)==-1)/*error*/exit(1);
 
-	n = bind(fd, res->ai_addr, res->ai_addrlen);
-	if (n == -1) /* error */ exit(1);
-
-	if (listen(fd, 5) == -1) /* error */ exit(1);
-
-	while (1) {
-		addrlen = sizeof(addr);
-		if ((newfd = accept(fd, (struct sockaddr*)&addr, &addrlen)) == -1) /* error */ exit(1);
-
-		while((n=read(newfd,buffer,128))!=0) {
-			if(n==-1)/*error*/exit(1);
-			write(1,"echo: ",6);//stdout
-			write(1,buffer,n);
-			ptr = buffer;
-			/* ptr=&buffer[0]; */
-			while(n>0) {
-				if((nwritten=write(newfd,ptr,n))<=0)/*error*/exit(1);
-				n-=nwritten; ptr+=nwritten;
-			}
+	while(1){addrlen=sizeof(addr);
+		if((newfd=accept(fd,(struct sockaddr*)&addr,&addrlen))==-1)
+			/*error*/exit(1);
+		while((n=read(newfd,buffer,128))!=0){if(n==-1)/*error*/exit(1);
+			ptr=&buffer[0];
+			write(1,"received: ",10);
+			write(1,ptr,n);
+			while(n>0){if((nw=write(newfd,ptr,n))<=0)/*error*/exit(1);
+				n-=nw; ptr+=nw;}
 		}
-
 		close(newfd);
 	}
-
-	freeaddrinfo(res);
-	close(fd);
+	freeaddrinfo(res);close(fd);exit(0);
 }
 
