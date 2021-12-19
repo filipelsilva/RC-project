7ede2b3c186fffb257aeb41113b127e37dbb65ad
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-19 23:16:22 +0000

    changed header files

--- server/udp_server.cpp
+++ server/udp_server.cpp
@@ -1,11 +1,14 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <cstdlib>
+#include <cstring>
+#include <csignal>
+extern "C" {
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
+}
 
 #define PORT "58001"
 
