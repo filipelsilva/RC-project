97115c786e32e817cfe82334bc720d4b43644190
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-23 21:02:15 +0000

    all servers working as of now, improvements to be made but still

--- client/TCPClient.cpp
+++ client/TCPClient.cpp
@@ -1,59 +1,86 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <csignal>
+extern "C" {
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <signal.h>
+#include <unistd.h>
+}
 
-int main(void) {
+class TCPClient {
 	struct sigaction act;
 	struct addrinfo hints, *res;
-	int fd, n;
+	int fd, n, errcode;
 	ssize_t nbytes, nleft, nwritten, nread;
 	char *ptr, buffer[128];
+	const char *server, *port;
 
-	memset(&act, 0, sizeof(act));
-	act.sa_handler = SIG_IGN;
-	if (sigaction(SIGPIPE, &act, NULL) == -1) exit(1);
+	public:
+	TCPClient(const char *server, const char *port) {
+		// Handling of SIGPIPE signal
+		memset(&act, 0, sizeof(act));
+		act.sa_handler = SIG_IGN;
+		if ((errcode = sigaction(SIGPIPE, &act, NULL)) == -1) {
+			fprintf(stderr, "Error: sigaction: %s\n", gai_strerror(errcode));
+			exit(1);
+		}
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd == -1) exit(1);
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
+		// Server setup
+		this->server = server;
+		this->port = port;
 
-	n = getaddrinfo("localhost", "58001", &hints, &res);
-	if (n != 0) exit(1);
-	n = connect(fd, res->ai_addr, res->ai_addrlen);
-	if (n == -1) exit(1);
+		if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+			fprintf(stderr, "Error: socket: %s\n", gai_strerror(fd));
+			exit(1);
+		}
 
-	ptr = strcpy(buffer, "Hello!\n");
-	nbytes = 7;
+		memset(&hints, 0, sizeof hints);
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
 
-	nleft = nbytes;
-	while (nleft > 0) {
-		nwritten = write(fd, ptr, nleft);
-		if (nwritten <= 0) exit(1);
-		nleft -= nwritten;
-		ptr += nwritten;
-	}
-	nleft = nbytes;
-	ptr = buffer;
-
-	while (nleft > 0){
-		nread = read(fd, ptr, nleft);
-		if(nread == -1) exit(1);
-		else if (nread == 0) break;
-		nleft -= nread;
-		ptr += nread;
+		n = getaddrinfo("localhost", "58001", &hints, &res);
+		if (n != 0) exit(1);
+		n = connect(fd, res->ai_addr, res->ai_addrlen);
+		if (n == -1) exit(1);
 	}
 
-	nread = nbytes - nleft;
+	void sendData(const char *message) {
+		ptr = strcpy(buffer, message);
+		nbytes = strlen(message);
 
-	write(1, "echo: ", 6);
-	write(1, buffer, nread);
+		nleft = nbytes;
+		while (nleft > 0) {
+			nwritten = write(fd, ptr, nleft);
+			if (nwritten <= 0) exit(1);
+			nleft -= nwritten;
+			ptr += nwritten;
+		}
+		nleft = nbytes;
+		ptr = buffer;
 
-	freeaddrinfo(res);
-	close(fd);
+		while (nleft > 0){
+			nread = read(fd, ptr, nleft);
+			if(nread == -1) exit(1);
+			else if (nread == 0) break;
+			nleft -= nread;
+			ptr += nread;
+		}
+
+		nread = nbytes - nleft;
+
+		write(1, "echo: ", 6);
+		write(1, buffer, nread);
+	}
+
+	~TCPClient() {
+		freeaddrinfo(res);
+		close(fd);
+	}
+};
+
+int main(int argc, char **argv) {
+	TCPClient client = TCPClient("localhost", "58001");
+	client.sendData("Teste de servidor UDP\n");
 }
