79bd88bf6045b3cacd7fb79a5a68903cbbb1a815
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-01-13 14:24:04 +0000

    added some parts

--- client/user.cpp
+++ client/user.cpp
@@ -190,6 +190,7 @@ int main(int argc, char **argv) {
 
 		if(isTCP(cmd.assign(input))){
 			TCPClient tcp = TCPClient(DSIP, DSport);
+			tcp.createSocketAndConnect();
 
 			cmd = processTCPCommand(input, tcp);
 
@@ -199,6 +200,7 @@ int main(int argc, char **argv) {
 			}
 			else{
 			}
+			tcp.closeConnection();
 		}
 
 		else if(isUDP(cmd)){
